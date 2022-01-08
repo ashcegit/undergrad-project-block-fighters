31cce2e61ed7efeaac8a2da05315026160faee6c
Ashley Gittens <ag554@student.le.ac.uk>
2022-01-08 20:36:10 +0000

    Created Maths functions

---
+++ Battle System Prototype/Assets/BlockProgramming/BlockScripts/Functions/Math Functions/MathMultiplyFunction.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathMultiplyFunction : MathFunction
+{
+    public override float function(){
+        List<InputFieldHandler> inputFieldHandlers=gameObject.GetComponent<Block>().getSections()[0].getHeader().getInputFieldHandlers();
+        MathFunction mathFunction0=inputFieldHandlers[0].getInputBlock().GetComponent<MathFunction>();
+        MathFunction mathFunction1=inputFieldHandlers[1].getInputBlock().GetComponent<MathFunction>();
+        return(mathFunction0.function()*mathFunction1.function());
+    }
+}
