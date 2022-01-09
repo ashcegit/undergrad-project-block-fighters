494ee293301bd00319a48e636be1ca816ff67b25
Ashley Gittens <ag554@student.le.ac.uk>
2022-01-09 13:26:03 +0000

    Repeat X times functional

--- Battle System Prototype/Assets/BlockProgramming/BlockScripts/Functions/Control Functions/ControlRepeatFunction.cs
+++ Battle System Prototype/Assets/BlockProgramming/BlockScripts/Functions/Control Functions/ControlRepeatFunction.cs
@@ -10,17 +10,24 @@ public class ControlRepeatFunction : ControlFunction
     public override int function(int pointer,ref List<Block> blockStack){
         Block thisBlock=gameObject.GetComponent<Block>();
         List<InputFieldHandler> inputFieldHandlers=gameObject.GetComponent<Block>().getSections()[0].getHeader().getInputFieldHandlers();
-        MathFunction mathFunction=inputFieldHandlers[0].getInputBlock().GetComponent<MathFunction>();
-        int arg=(int)mathFunction.function();
+        int operand=0;
+        Block operandBlock=inputFieldHandlers[0].getInputBlock().GetComponent<Block>();
+        if(operandBlock.getBlockType()==BlockType.Math){
+            operand=(int)operandBlock.gameObject.GetComponent<MathFunction>().function();
+        }else if(operandBlock.getBlockType()==BlockType.Info){
+            operand=(int)operandBlock.gameObject.GetComponent<InfoFunction>().function();
+        }
+        pointer++;
         int endPointer=pointer;
         List<Block> subBlockStack=new List<Block>();
-        while(blockStack[endPointer]!=thisBlock){
+        while(blockStack[endPointer].getStartBlock()!=thisBlock){
             endPointer++;
         }
         subBlockStack=blockStack.GetRange(pointer,endPointer);
-        for(int i=0;i<arg;i++){
+        for(int i=0;i<operand;i++){
+            Debug.Log("Adding");
             blockStack.InsertRange(pointer,subBlockStack);
         }
-        return ++pointer;
+        return pointer;
     }
 }
